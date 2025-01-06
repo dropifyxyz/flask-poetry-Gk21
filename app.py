de565000c3099c578edb31eb3ba77ff137c1accc
Matthew Lal <3614683+matthewlal@users.noreply.github.com>
2025-01-06 23:42:28 +0000

    made async because of deploy issue

--- app.py
+++ app.py
@@ -1,29 +1,23 @@
 from flask import Flask, jsonify
-from dendrite_sdk import Dendrite
+from dendrite_sdk import AsyncDendrite
+from dendrite_sdk.remote import BrowserbaseConfig
 import subprocess
 
 app = Flask(__name__)
 
 @app.route('/')
-def run_dendrite_auth():
+async def run_dendrite_auth():
     try:
-        # This will open the local browser
-        process = subprocess.Popen(
-            ["dendrite", "auth", "--url", "linkedin.com"],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE
-        )
-        stdout, stderr = process.communicate()
+        browser = AsyncDendrite(auth=["youtube.com"], dendrite_api_key="sk_d6aacf77ee8acba13ce14c8da02e1c7f22e7c9b83451ffb80ed7b2a9b5f97028", playwright_options={ "headless": True})
 
-        browser = Dendrite(auth=["facebook.com", "instagram.com", "tiktok.com", "youtube.com", "linkedin.com"], dendrite_api_key="sk_d6aacf77ee8acba13ce14c8da02e1c7f22e7c9b83451ffb80ed7b2a9b5f97028", playwright_options={ "headless": True})
-
-        browser.goto(
-            "https://linkedin.com",
-            expected_page="You should be logged in with the linkedin feed visible",
+        await browser.goto(
+            "https://youtube.com",
+            expected_page="You should be logged in with the youtube feed visible",
         )
-        desc = browser.ask("Who was ")
+        desc = await browser.ask("List the recommended video topics")
         print("First post description: ", desc)
 
+        await browser.close()
         
         return jsonify({
             "success": True,
