f6b140035a3c96d4a0aeb45f1fa0976f32e35ec8
Matthew Lal <3614683+matthewlal@users.noreply.github.com>
2025-01-07 00:07:19 +0000

    changed api to railway env var

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify
 from dendrite_sdk import AsyncDendrite
 from dendrite_sdk.remote import BrowserbaseConfig
-import subprocess
+import os
 
 app = Flask(__name__)
 
@@ -12,13 +12,13 @@ async def health_check():
 @app.route('/')
 async def run_dendrite_auth():
     try:
-        browser = AsyncDendrite(auth=["youtube.com"], dendrite_api_key="sk_d6aacf77ee8acba13ce14c8da02e1c7f22e7c9b83451ffb80ed7b2a9b5f97028", playwright_options={ "headless": True})
+        browser = AsyncDendrite(auth=["youtube.com"], dendrite_api_key=os.getenv("DENDRITE_API_KEY"), playwright_options={ "headless": True})
 
         await browser.goto(
             "https://youtube.com",
             expected_page="You should be logged in with the youtube feed visible",
         )
-        desc = await browser.ask("List the recommended video topics")
+        desc = await browser.ask("List the recommended videos")
         print("First post description: ", desc)
 
         await browser.close()
